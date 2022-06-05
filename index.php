f87e6000d1da895a8f939db4f23a6acfeb03df27
Francis Akabuogu <fakabuogu@gmail.com>
2022-06-05 10:18:13 +0000

    complete

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST,- DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Auth-Token");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: http://localhost:3000");
+    header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Auth-Token");
+    exit(200);
+}
+
 
 require __DIR__ . "/inc/bootstrap.php";
 
